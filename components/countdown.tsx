7884aa472ec842f45f370f527a8090f78b0aa034
Minh Khang Tran <67480568+MinhKhangTran@users.noreply.github.com>
2022-07-21 15:48:15 +0200

    :sparkles: addcontent

--- components/countdown.tsx
+++ components/countdown.tsx
@@ -1,3 +1,11 @@
+// Countdown app
+// https://blog.greenroots.info/how-to-create-a-countdown-timer-using-react-hooks
+
+const countdownContent = {
+    text: "Das Event startet in:",
+    targetDate : 1814000000
+  };
+
 const Countdown = () => {
     return (
         <section className="" id="countdown" aria-label="countdown Abschnitt">
