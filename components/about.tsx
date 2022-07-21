9150d170585497a982319e668f9b08edee2cf6be
Minh Khang Tran <67480568+MinhKhangTran@users.noreply.github.com>
2022-07-21 16:51:36 +0200

    :sparkles: add content

--- components/about.tsx
+++ components/about.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+import aboutPic from "../public/images/food-truck-fest.jpg";
+
+
+
+const heroContent = {
+        altText: "Ein Bild von einem Pärchen bei dem Food Truck Fest",
+        text:"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit ac aliquet morbi accumsan, arcu ullamcorper purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit ac aliquet morbi accumsan, arcu ullamcorper purus.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit ac aliquet morbi accumsan, arcu ullamcorper purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit ac aliquet morbi accumsan, arcu ullamcorper purus.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit ac aliquet morbi accumsan, arcu ullamcorper purus.</p>"
+      };
+
+
 const About = () => {
     return (
         <section className="" id="about" aria-label="about Abschnitt">
--- components/sponsor.tsx
+++ components/sponsor.tsx
@@ -1,3 +1,39 @@
+const sponsorArray = [
+    {
+      id: 1,
+      thumb: "rennende-schuhe.svg",
+      altText: "Ein Bild von Peter Kim",
+      title: "Peter Kim",
+      essen: "Koreanisches Essen",
+      text: "Cat ipsum dolor sit amet, malkin yet ocicat. Tomcat donskoy. Kitten. Havana brown russian blue lion turkish angora british shorthair, egyptian mau. Persian tomcat himalayan kitty. Thai sphynx british shorthair yet kitten burmese.",
+    },
+    {
+      id: 2,
+      thumb: janeDoe,
+      altText: "Ein Bild von Jane Doe",
+      title: "Jane Doe",
+      essen: "Amerikanisches Essen",
+      text: "Ocelot ragdoll or norwegian forest cougar, tiger or tabby kitten. Abyssinian munchkin so devonshire rex. British shorthair ocicat but kitten. Malkin kitten and american shorthair maine coon leopard. ",
+    },
+    {
+      id: 3,
+      thumb: cathyBaros,
+      altText: "Ein Bild von Cathy Baros",
+      title: "Cathy Baros",
+      essen: "Griechisches Essen",
+      text: "Jaguar siberian so puma but ragdoll yet ragdoll lion scottish fold. Leopard. Tiger scottish fold or persian thai munchkin. Devonshire rex abyssinian panther norwegian forest lynx. Panther norwegian forest burmese and balinese . American shorthair.",
+    },
+    {
+      id: 4,
+      thumb: albertoRossi,
+      altText: "Ein Bild von Alberto Rossi",
+      title: "Alberto Rossi",
+      essen: "Italienisches Essen",
+      text: "American shorthair turkish angora or kitty, or malkin bengal. Panther abyssinian , for lynx. Bengal norwegian forest. Panther ocicat so ocicat tiger tabby. Tomcat egyptian mau yet panther, leopard or jaguar. Cornish rex himalayan and cornish rex for bengal but jaguar yet burmese yet bengal.",
+    }
+  ];
+
+
 const Sponsor = () => {
     return (
         <section className="" id="sponsor" aria-label="sponsor Abschnitt">
