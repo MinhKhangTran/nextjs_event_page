dd7788101b2f8881ced54fc614e8cdf8a37f01e5
Minh Khang Tran <67480568+MinhKhangTran@users.noreply.github.com>
2022-07-21 15:38:57 +0200

    :sparkles: header and hero start

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,3 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import heroPic from "../public/images/hero.png";
+
+
+// SVG for next
+// https://frontend-digest.com/how-to-import-svgs-into-nextjs-8ec6100e613f
+
+
+const heroContent = {
+        altText: "Ein Bild von einer Frau in einem Food Truck, die eine Kundin bedient",
+        title: "Food Truck Fest 2022",
+        btnText: "Erfahre mehr",
+        time_and_location:[
+                {
+                        id:1,
+                        icon: "calendar.svg",
+                        text:"08. - 10.08.2022"
+
+                },
+                {
+                        id:2,
+                        icon: "location.svg",
+                        text:"Friedrichsau, Ulm"
+
+                }
+        ]
+      };
+
 const Hero = () => {
     return (
         <section className="" id="hero" aria-label="hero Abschnitt">
