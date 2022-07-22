8f4d98c8fdfda30f5a1fb725827da9de5c6124c1
Minh Khang Tran <67480568+MinhKhangTran@users.noreply.github.com>
2022-07-22 15:02:43 +0200

    :sparkles: about section

--- components/about.tsx
+++ components/about.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import aboutPic from "../public/images/food-truck-fest.jpg";
+import { Col, Row } from "react-bootstrap";
 
 
 
-const heroContent = {
+const aboutContent = {
         altText: "Ein Bild von einem Pärchen bei dem Food Truck Fest",
         text:"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit ac aliquet morbi accumsan, arcu ullamcorper purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit ac aliquet morbi accumsan, arcu ullamcorper purus.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit ac aliquet morbi accumsan, arcu ullamcorper purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit ac aliquet morbi accumsan, arcu ullamcorper purus.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hendrerit ac aliquet morbi accumsan, arcu ullamcorper purus.</p>"
       };
@@ -12,8 +13,21 @@ const heroContent = {
 
 const About = () => {
     return (
-        <section className="" id="about" aria-label="about Abschnitt">
-                About
+        <section className="wrapper" id="about" aria-label="about Abschnitt">
+                <Row>
+                    <Col md={6}>
+                    <Image
+            src={aboutPic}
+            alt={aboutContent.altText}
+            className="img-fluid"
+            placeholder="blur"
+            priority
+          />
+                    </Col>
+                    <Col md={6}>
+                    <div dangerouslySetInnerHTML={{ __html: aboutContent.text }}></div>
+                    </Col>
+                </Row>
         </section>  
             );
         };
