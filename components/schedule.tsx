cccd40106ebaf916d3115681ec5c4ade1472bc78
MinhKhangTran <minhkhang.tran@gmx.de>
2022-07-24 12:06:45 +0200

    :sparkles: start with schedule

--- components/schedule.tsx
+++ components/schedule.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import aufbau from "../public/images/aufbau.jpg";
 import liveMusic from "../public/images/live-music.jpg";
 import abschiedFeuerwerk from "../public/images/abschied-feuerwerk.jpg";
-
-
+import { Col, Row, Tab, Tabs } from "react-bootstrap";
+import { useState } from "react";
 
 const scheduleArray = [
   {
@@ -34,9 +34,36 @@ const scheduleArray = [
 ];
 
 const Schedule = () => {
+  const [index, setIndex] = useState(1);
   return (
-    <section className="" id="schedule" aria-label="schedule Abschnitt">
-      Schedule
+    <section className="wrapper" id="schedule" aria-label="schedule Abschnitt">
+      <Row>
+        <Col md={6}>
+          <Image
+            src={scheduleArray[index - 1].thumb}
+            alt={scheduleArray[index - 1].altText}
+            className="img-fluid"
+            placeholder="blur"
+            priority
+          />
+        </Col>
+        <Col md={6}>
+          <Tabs activeKey={index} onSelect={(k) => setIndex(k)}>
+            {scheduleArray.map((schedule) => {
+              return (
+                <Tab
+                  eventKey={schedule.id}
+                  key={schedule.id}
+                  title={schedule.date}
+                >
+                  <h3 className="heading-3">{schedule.title}</h3>
+                  <p>{schedule.text}</p>
+                </Tab>
+              );
+            })}
+          </Tabs>
+        </Col>
+      </Row>
     </section>
   );
 };
